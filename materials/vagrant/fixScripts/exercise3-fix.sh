3ac28137ecb1d7e4f1cb5ba12926ac001bd260d8
P4N3R-rg <refaelg@protonmail.com>
2023-08-28 16:39:51 +0300

    solve challenge

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 #add fix to exercise3 here
+
+# Define the path to the Apache configuration file
+apache_config="/etc/apache2/sites-enabled/000.-default.conf"  # Replace with the actual path
+
+# Check if the Apache config file exists
+if [ -f "$apache_config" ]; then
+    # Modify the Require directive within the Location block
+    sudo sed -i '/<Location "\/">/,/<\/Location>/ s/Require all denied/Require all granted/' "$apache_config"
+
+    echo "Apache configuration updated."
+    sudo service apache2 restart
+else
+    echo "Apache configuration file not found."
+fi
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+
+# Edit /etc/hosts file
+echo "192.168.60.10 server1" >> /etc/hosts
+
+sudo systemctl restart systemd-resolved.service
+
